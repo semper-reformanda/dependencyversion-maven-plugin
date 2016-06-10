e5ddaaf15c5505acd291a99b13819fe11248d68c
Josh Cain <jcain@redhat.com>
2016-06-09 21:01:03 -0500

    convert javadoc-based mojo configs to annotations

--- dependencyversion-maven-plugin/src/main/java/cain/josh/maven/dependencyVersion/DependencyPathMojo.java
+++ dependencyversion-maven-plugin/src/main/java/cain/josh/maven/dependencyVersion/DependencyPathMojo.java
@@ -20,6 +20,10 @@ import org.apache.maven.artifact.Artifact;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
 
 import java.io.File;
@@ -31,38 +35,31 @@ import java.util.Set;
 /**
  * Sets a property pointing to the artifact version for each selected project dependency.
  * Each property name will have a base name in form of groupId:artifactId:type:[classifier][.relative][.suffix].
- *
- * @author Josh Cain
- * @requiresDependencyResolution test
- * @goal set
- * @phase initialize
- * @threadSafe
  */
+@Mojo(name = "set-version",
+    defaultPhase = LifecyclePhase.INITIALIZE,
+    requiresDependencyResolution = ResolutionScope.TEST,
+    threadSafe = true)
 public class DependencyPathMojo extends AbstractMojo {
-    /**
-     * @parameter expression = "${project}"
-     * @required
-     * @readonly
-     * @since 1.0.0
-     */
+
+    @Parameter(alias = "project",
+            property = "project",
+            required = true,
+            readonly = true)
     private MavenProject project;
 
     /**
      * The default property set.
      * See <a href="propertyset-reference.html">PropertySet Reference</a>.
-     *
-     * @parameter
-     * @since 1.0.0
      */
+    @Parameter
     private PropertySet defaultPropertySet;
 
     /**
      * The property sets.
      * See <a href="propertyset-reference.html">PropertySet Reference</a>.
-     *
-     * @parameter
-     * @since 1.0.0
      */
+    @Parameter
     private PropertySet[] propertySets;
 
     public void execute() throws MojoExecutionException, MojoFailureException {
